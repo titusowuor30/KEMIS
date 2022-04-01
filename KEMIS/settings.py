ee36187fd0fffa79142877c1fa376b7fc2ae8e4b
Titus Owuor <titusowuor30@gmail.com>
2022-04-01 14:01:57 +0300

    updates

--- KEMIS/settings.py
+++ KEMIS/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'managemenet',
+    'srapper',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'KEMIS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'kemis',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'root',
+        'PORT':'3307'
     }
 }
 
--- managemenet/models.py
+++ managemenet/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Industry(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(max_length=255)
+    location=models.CharField(max_length=255)
+    inputs=models.ForeignKey('Inputs',on_delete=models.CASCADE)
+    waste_product=models.ForeignKey('WasteProducts',on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural='Industries'
+    def __str__(self):
+        return self.name   
+    
+class Inputs(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField(max_length=255)
+    
+    class Meta:
+        verbose_name_plural='Inputs'
+    def __str__(self):
+        return self.name 
+    
+class WasteProducts(models.Model):
+     name=models.CharField(max_length=200)
+     description=models.TextField(max_length=255)
+     
+     class Meta:
+            verbose_name_plural='Waste Products'
+     def __str__(self):
+        return self.name 
+          
+        
